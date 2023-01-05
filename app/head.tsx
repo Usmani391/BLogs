c1dd2ea942326f494859eb3fffab1d92870f6bce
Usman <manidevolper96@gmail.com>
2023-01-05 19:18:35 +0500

    head

--- app/head.tsx
+++ app/head.tsx
@@ -2,8 +2,8 @@
 function head() {
   return (
     <>
-      <title>Turbo Pack</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title><i>Usman'i Blogs</i></title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" >
       <link  rel='icon' href='/favicon.ico'/>
     </>
     
