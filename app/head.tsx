e5f379f2af5dbf4fe1722399482548dca3ad08ee
usmani <114348374+Usmani391@users.noreply.github.com>
2023-01-05 19:32:00 +0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@
 function head() {
   return (
     <>
-      <title><i>Usman'i Blogs</i></title>
+      <title>Usman'i Blogs</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link  rel='icon' href='/favicon.ico'/>
     </>
