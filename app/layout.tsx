0563b3677ea379071278111b0dcc80a240ca2b0a
Usman <manidevolper96@gmail.com>
2023-01-05 18:10:14 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,4 @@
 import '@/styles/globals.css';
-import React from 'react';
-import AddressBar from '@/ui/AddressBar';
-import GlobalNav from './GlobalNav';
 
 export default function RootLayout({
   children,
@@ -16,11 +13,11 @@ export default function RootLayout({
       <body className="overflow-y-scroll bg-zinc-900">
         <div className="grid grid-cols-[1fr,minmax(auto,240px),min(800px,100%),1fr] gap-x-8 py-8">
           <div className="col-start-2">
-            <GlobalNav />
+            {/* <GlobalNav /> */}
           </div>
 
           <div className="col-start-3 space-y-6">
-            <AddressBar />
+            {/* <AddressBar /> */}
 
             <div className="rounded-xl border border-zinc-800 bg-black p-8">
               {children}
