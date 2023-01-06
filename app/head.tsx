718e1e71c96cb422996b196829e394c4bd3f451f
Usman <manidevolper96@gmail.com>
2023-01-06 08:27:15 +0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,11 +2,10 @@
 function head() {
   return (
     <>
-      <title><i>Usman'i Blogs</i></title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+      <title>Usman'i Blogs</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <link  rel='icon' href='/favicon.ico'/>
-    </>
-    
+    </> 
   )
 }
 
